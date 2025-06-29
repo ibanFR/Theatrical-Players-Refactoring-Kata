9357c643bacd1481ddf838feac914147dbedb72b
ibanFR <ibanfernandezrojo@msn.com>
2025-06-29 21:03:42 +0100

    refactor: rename parameter to performance in PerformanceData record

--- java/src/main/java/theatricalplays/PerformanceData.java
+++ java/src/main/java/theatricalplays/PerformanceData.java
@@ -1,12 +1,12 @@
 package theatricalplays;
 
-public record PerformanceData(Performance aPerformance, Play play) {
+public record PerformanceData(Performance performance, Play play) {
     int volumeCreditsFor() {
         int result = 0;
-        result += Math.max(aPerformance().audience - 30, 0);
+        result += Math.max(performance().audience - 30, 0);
         // add extra credit for every ten comedy attendees
         if ("comedy".equals(play().type))
-            result += Math.floor(aPerformance().audience / 5);
+            result += Math.floor(performance().audience / 5);
         return result;
     }
 }
